657c290a55128f65689362e0ca0c02d31cbb9471
vhusername123 <github1@hoffvinc.de>
2024-12-17 10:54:51 +0100

    2nd commit

--- main.ts
+++ main.ts
@@ -1,20 +1,46 @@
 import { stdin } from "node:process";
+type MyDay = {
+    year:number;
+    month:number;
+    day:number;
+};
+type MyTime = {
+    hours:number;
+    minutes:number;
+    seconds:number;
+};
 
-// /media/vincent/1817-19D5/Work/log/2024_12_16log.text
+type Workday = {
+    day: MyDay;
+    workinghours: number;
+};
+
+type Workweek = {
+    week: Workday[];
+    sumWorkinghours: number;
+};
+
+// testdates.text
 const readByLine = (path:string) => Deno.readTextFile(path).then((message) => {
 
     const array = message.split(/[\r\n]+/);
     array.forEach(element => {
         if(element.length > 1){
-            const temp:Date = new Date(
-                Number(element.substring(0,4)),
-                Number(element.substring(4,6)),
-                Number(element.substring(6,8)),
-                Number(element.substring(8,10)),
-                Number(element.substring(11,13)),
-                Number(element.substring(14,16)),
-            );
-            console.log(temp);
+            let currentworkday:Workday = {
+                day:
+                    {
+                        year: Number(element.substring(0,4)),
+                        month: Number(element.substring(4,6)),
+                        day: Number(element.substring(6,8)),
+                    },
+                workinghours: 0
+            };
+            const currenttime:MyTime = {               
+                hours: Number(element.substring(8,10)),
+                minutes: Number(element.substring(11,13)),
+                seconds: Number(element.substring(14,16)),
+            };
+            
         };
     });
 
